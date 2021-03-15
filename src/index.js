fbce6a5b34abb8cca67d5aa05168dbdf83a6c4f0
Antoine Ando <antoine.ando@gmail.com>
2021-03-15 23:50:33 +0100

    basic memory behavior

--- src/index.js
+++ src/index.js
@@ -36,22 +36,50 @@ class Game {
 
 			//Si aucune carte n'a été sélectionnée
 			if(this.firstCard == undefined){
+				console.log("Première carte")
+				console.log(e.target)
+				e.target.classList.add("selected");
+
 				this.firstCard = {
-					"pair"  : e.target.getAttribute("pair"),
-					"value" : e.target.innerHTML
+					"element" 	: e.target,
+					"pair"  	: e.target.getAttribute("pair"),
+					"value" 	: e.target.innerHTML
 				}
 			}else{
+				console.log("Deuxième carte")
+				console.log(e.target)
+
 				//Si l'utilisateur clique sur la même carte
 				if(e.target.getAttribute('pair') == this.firstCard.pair && e.target.innerHTML == this.firstCard.value){
 					console.log("Même carte");
 					return;
 
 				}else if(e.target.innerHTML == this.firstCard.value && e.target.getAttribute('pair') != this.firstCard.pair){
+					e.target.classList.add("selected");
 					console.log("nice")
 
+					//Les cartes restent affichées quelques secondes avant d'être retournées et grisées
+					setTimeout(()=>{
+						let card1 = document.getElementsByClassName('selected').item(0)
+						card1.classList.remove("selected");
+						card1.classList.add("found")
+
+						let card2 = document.getElementsByClassName('selected').item(0)
+						card2.classList.remove("selected");
+						card2.classList.add("found")
+
+					}, 1000)
+
 				}else if(e.target.innerHTML != this.firstCard.value && e.target.getAttribute('pair') != this.firstCard.pair){
+					e.target.classList.add("selected");
 					console.log("Mauvaise paire");
-					
+
+					//Les cartes restent affichées quelques secondes avant d'être retournées
+					setTimeout(()=>{
+						document.getElementsByClassName('selected').item(0).classList.remove("selected")
+						document.getElementsByClassName('selected').item(0).classList.remove("selected")
+					}, 1000)
+
 				}
 				this.firstCard = undefined;
 			}
