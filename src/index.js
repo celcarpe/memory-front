bb277b58e638a38442a5841a4404fca7fdd76e54
Antoine Ando <antoine.ando@gmail.com>
2021-03-16 20:39:54 +0100

    improved game mechanics with WIN and RESTART

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
 import './index.css';
 
+
+/*
+
+TODO
+- Timer
+- Progress bar
+- BDD
+
+In progress
+- mécanique de partie (début, victoire, etc...)
+
+
+*/
+
 class Game {
 
 	constructor(){
@@ -8,12 +22,23 @@ class Game {
 		this.gameID = this._generateGameID(10);
 
 		this.cards = this._generateCards(5)
+		this.pairNumber = 5;
 
+		//récupération des éléments du DOM
 		this.boardElement = document.getElementById("board");
-		this.canPlay = true;
+		this.playButton = document.getElementById("playButton");
+		this.restartButton = document.getElementById("restart")
+
 	}
 
 	initializeBoard(){
+
+		//Réinitialisation de la board
+		this.boardElement.innerHTML = "";
+		this.restartButton.classList.add("hidden");
+		this.playButton.classList.remove("hidden");
+
+		this.canPlay = false;
 
 		//Génération du HTML des cartes
 		var cardsString = this.cards.reduce((accumulator, card) => {
@@ -23,9 +48,25 @@ class Game {
 		//Insertion des cartes dans le plateau
 		this.boardElement.insertAdjacentHTML("beforeend", cardsString );
 
+
 		this.boardElement.addEventListener("click", this._checkClick.bind(this) );
+		this.playButton.addEventListener("click", this.startGame.bind(this));
+		this.restartButton.addEventListener("click", this.restartGame.bind(this));
 
 	}
+
+	startGame(e){
+		this.playButton.classList.add("hidden")
+		//start timer
+		this.canPlay = true;
+
+		this.pairFound = 0;
+	}
+
+	restartGame(e){
+		this.initializeBoard();
+	}
+
 
 	set setFirstCard(card){
 		this.firstCard = card;
@@ -51,27 +92,33 @@ class Game {
 
 				//Si l'utilisateur clique sur la même carte
 				if(e.target.getAttribute('pair') == this.firstCard.pair && e.target.innerHTML == this.firstCard.value){
+					//Si même carte
 					return;
 
 				}else if(e.target.innerHTML == this.firstCard.value && e.target.getAttribute('pair') != this.firstCard.pair){
-					e.target.classList.add("selected");
+					//Si bonne carte
 					this.canPlay = false;
+					this.pairFound += 1;
+
+					e.target.classList.add("selected");
 
 					//Les cartes restent affichées quelques secondes avant d'être retournées et grisées
 					setTimeout(()=>{
 						let card1 = document.getElementsByClassName('selected').item(0)
 						card1.classList.remove("selected");
-						card1.classList.add("found")
+						card1.classList.add("hidden")
 
 						let card2 = document.getElementsByClassName('selected').item(0)
 						card2.classList.remove("selected");
-						card2.classList.add("found")
+						card2.classList.add("hidden")
 
 						this.canPlay = true;
 
 					}, 1000)
 
 				}else {
+					//Si mauvaise carte
+
 					e.target.classList.add("selected");
 					this.canPlay = false;
 
@@ -87,6 +134,15 @@ class Game {
 				this.firstCard = undefined;
 			}
 		}
+
+		if(this.pairFound == this.pairNumber){
+			this._victory();
+		}
+	}
+
+	_victory(){
+		alert("Victoire");
+		this.restartButton.classList.remove("hidden");
 	}
 
 	_generateGameID(length) {
